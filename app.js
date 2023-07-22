4d85ffd6497c10ac58acbb574d69ea696a8d6af7
Eguar <eguar.student@gmail.com>
2023-07-22 19:29:35 +0300

    4.13 Арефмитические опперации

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-let a = "var 1";
-const b = "var 2";
-a = "Var 3";
-b = "Var 4";
-console.log(a);
-console.log(b);
+// Базовые Арефмитические операции
+const width=10;
+const height=5;
+const newwidth = width-4;
+const newwidth2 = width+4;
+const space=width*height;
+const division = newwidth / newwidth2
+const step = width ** 5; // Width в степени 5
+console.log(step);
+
+// Строки
+const city = "Odessa";
+const street = "Марсельская";
+const address = city+", "+street; // Конкотенация - Обьеденение строк
+const address2 = address + " "+5 // Обьеденение строки и числа - дает строку
+console.log(address2);
